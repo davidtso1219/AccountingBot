ec5930e24083b0e5b40b24ed8b8a526d6df0bf9e
david tso <davidtso1219@gmail.com>
2022-03-25 12:27:56 -0700

    Changed port to heroku's config

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,7 @@ def home():
 	return "I'm alive"
 
 def run():
-	app.run(host='0.0.0.0',port=8080)
+	app.run(host='0.0.0.0',port=os.getenv('PORT', 8080))
 
 def keep_alive():
 	print("keep alive..")
