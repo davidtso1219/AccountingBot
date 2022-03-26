a24fd36a8a599f05f918daa125dd458f95386649
david tso <davidtso1219@gmail.com>
2022-03-26 12:11:51 -0700

    Upgraded utilites for more accurate functionalities

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import pytz
-from discord import Color
+from discord import Color, Embed
 from datetime import datetime, timedelta
 
 def get_args(info, columns):
@@ -8,35 +8,50 @@ def get_args(info, columns):
         args.append(info[c])
     return args
 
-async def validate_author(author, msg, embed):
-    '''
-    '''
-    if author != 'daaviid' and author != 'Jamie.lin':
-        embed.description = f':warning: You are **NOT** supposed to add!'
-        embed.color = Color.from_rgb(255, 0, 0)
-        await msg.edit(embed=embed)
-        return False
+def check_author(author):
+    return author == 'daaviid' or author == 'Jamie.lin'
 
-    return True
+async def send_red_warning(msg, embed, description):
+    await msg.edit(embed=Embed(description=description, color=Color.from_rgb(255, 0, 0)))
+    return
 
-async def get_time_info(date, author, msg, embed):
+def set_time_info(time_info, date, format):
+
+    date = datetime.strptime(date, format)
+    if format == '%m/%d/%Y' or format == '%m/%d/%y':
+        time_info['month'] = date.month
+        time_info['day'] = date.day
+        time_info['year'] = date.year
+
+    elif format == '%m/%Y':
+        time_info['month'] = date.month
+        time_info['day'] = None
+        time_info['year'] = date.year
+
+    elif format == '%m/%d':
+        time_info['month'] = date.month
+        time_info['day'] = date.day
+        time_info['year'] = datetime.now().year
+
+def get_time_info(author, date=''):
     '''
+    check if the date from the user is valid
     '''
-    # see if the date from the user is valid
+    time_info = {}
+    formats = ["%m/%d/%Y", "%m/%d/%y", "%m/%Y", "%m/%d"]
+
+    # if date is provided
     if date:
-        try:
-            datetime.strptime(date, "%m/%d/%Y")
-            time_info = date.split('/')
-        except ValueError:
-            embed.description = f':warning: `{date}` is **invalid**'
-            embed.color = Color.from_rgb(255, 0, 0)
-            await msg.edit(embed=embed)
-            return
+        for format in formats:
+            try:
+                set_time_info(time_info, date, format)
+                return time_info
+            except ValueError:
+                pass
 
     # get the current date
     else:
         n = 0 if author == 'daaviid' else 15
         today = datetime.now(pytz.timezone('US/Pacific')) + + timedelta(hours=n)
         time_info = today.strftime("%m/%d/%Y").split('/')
-
-    return time_info
+        return time_info
