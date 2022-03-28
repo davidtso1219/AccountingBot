4ac3db58785541f636451671a4c7e2196874bf1f
david tso <davidtso1219@gmail.com>
2022-03-27 17:15:34 -0700

    Fixed a typo

--- commands/add.py
+++ commands/add.py
@@ -119,7 +119,7 @@ class Add(commands.Cog):
         title = 'Choose categories from the following options'
         color = Color.blue()
         description = ''
-        cats = ['foods', 'transportation', 'entertainment', 'grocercy']
+        cats = ['foods', 'transportation', 'entertainment', 'grocery']
         n = len(cats)
 
         for i in range(n):
