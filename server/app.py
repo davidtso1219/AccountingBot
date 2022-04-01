fce4c749b4b6a70a2f98545f66a4ffb349cdea07
david tso <davidtso1219@gmail.com>
2022-04-01 11:41:48 -0700

    Added income and lending to categories and update Procfile

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import os
 from flask_cors import CORS
 from threading import Thread
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, send_from_directory
 
 app = Flask('Accounting Bot', static_folder='../frontend/dist')
 # app.cli.add_command(create_tables)
@@ -11,9 +11,6 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/')
 def home():
 	return send_from_directory(app.static_folder, 'index.html')
-	# response_object = {'status':'success'}
-	# response_object['data'] = 2022
-	# return jsonify(response_object)
 
 def run():
 	app.run(host='0.0.0.0',port=os.getenv('PORT', 8080))
